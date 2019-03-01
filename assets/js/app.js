e46f5c919d494a3fee0d78916d408d5e1aaae234
rajmahal <rmahal25@gmail.com>
2019-02-28 20:44:16 -0800

    took out nav bar working on hamburger bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,24 +3,32 @@ $( document ).ready(function() {
 
 
     $('a[href^="#"]').on('click', function (e) {
-      e.preventDefault();
+        e.preventDefault();
 
-      var target = this.hash,
-          $target = $(target);
+    var target = this.hash,
+        $target = $(target);
 
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top - 80
-      }, 900, 'swing', function () {
-          window.location.hash = target;
-      });
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 80
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+
+
+    $(function() {
+        $.getJSON('assets/jokes.json', function(data) {
+            var len = data.jokes.length
+            var counter = Math.floor(Math.random() * len);
+            var elem = document.getElementsByClassName("quote");
+            function change() {
+                $(elem).fadeTo(10000, 0, function() {
+                    this.innerHTML = data.jokes[counter];
+                    counter = ++counter % len;
+                    $(this).fadeTo(10000, 1, change)
+                })
+            }
+
+        });
+    });
   });
-
-
-
-
-
-
-
-  
-
 })
