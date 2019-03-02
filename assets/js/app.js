95d1c29b9685fc8ad8e1b2dca72b99f1a0eca1b7
rajmahal <rmahal25@gmail.com>
2019-03-01 23:46:06 -0800

    Working on mobile responsive and reading jokes from file

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,20 @@ $( document ).ready(function() {
     $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
 
-    var target = this.hash,
-        $target = $(target);
+        var target = this.hash,
+            $target = $(target);
 
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top - 80
-    }, 900, 'swing', function () {
-        window.location.hash = target;
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 80
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
     });
-
 
     $(function() {
         $.getJSON('assets/jokes.json', function(data) {
+            console.log("DATA:")
+            console.log(data)
             var len = data.jokes.length
             var counter = Math.floor(Math.random() * len);
             var elem = document.getElementsByClassName("quote");
@@ -30,5 +32,4 @@ $( document ).ready(function() {
 
         });
     });
-  });
 })
